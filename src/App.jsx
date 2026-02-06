e27c78d225f479d6c8ecbd73f50d4c401fa20207
Madasamy-123 <madasamy.asokan@ventragate.com>
2026-02-06 15:17:13 +0530

    initial commit setup react and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-center font-bold text-2xl">
+        ScaleBI Project
+      </h1>
+
+    </>
+  )
+}
+
+export default App
