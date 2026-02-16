bef0cae441414f3151e0c612d7dcdc9c271a5f0f
Madasamy-123 <madasamy.asokan@ventragate.com>
2026-02-16 17:00:31 +0530

    case study slides changes

--- src/sections/CaseStudy.jsx
+++ src/sections/CaseStudy.jsx
@@ -2,8 +2,54 @@ import StudyIcon from '../assets/icons/study-icon.svg';
 import FileIcon from '../assets/icons/file-icon.svg';
 import ToggleIcon from '../assets/icons/toggle-icon.svg';
 import SwitchIcon from '../assets/icons/switch-icon.svg';
+import { useState, useEffect } from 'react';
+import { Key } from 'lucide-react';
 
 export default function CaseStudy() {
+
+    const caseStudies = [
+        {
+            title: "Leading HR Tech Company Cut 80% of Manual Reporting Effort.",
+            description: "Replaced spreadsheets with a fully managed Apache Superset stack - automating 70+ HR reports in under 3 months.",
+            testimonial: "With ScaleBI, we finally own our analytics solution and still get enterprise support.",
+            author: "Director. HR Tech Company"
+        },
+        {
+            title: "FinTech Startup Reduced Dashboard Build Time by 65%.",
+            description:
+                "Built scalable BI infrastructure with role-based dashboards and real-time reporting.",
+            testimonial:
+                '"ScaleBI transformed our reporting workflow and reduced engineering dependency."',
+            author: "Head of Data. FinTech Startup",
+        }
+    ]
+
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [isTransitioning, setIsTransitioning] = useState();
+
+
+    // Always Right → Left only
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setActiveIndex((prev) => prev + 1);
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+
+    useEffect(() => {
+        if (activeIndex === caseStudies.length) {
+            setTimeout(() => {
+                setIsTransitioning(false);
+                setActiveIndex(0);
+            }, 700); // match transition duration
+        } else {
+            setIsTransitioning(true);
+        }
+    }, [activeIndex]);
+
+
     return (
         <section className="bg-[#FFFFFF] flex justify-center">
             <div className="w-full max-w-[1440px] flex flex-col items-center py-12 px-4 sm:px-8 lg:py-20 lg:px-20 lg:gap-[64px]">
@@ -22,44 +68,116 @@ export default function CaseStudy() {
                     </h2>
                 </div>
 
-                {/* Main Card */}
-                <div className="flex flex-col lg:flex-row bg-[#FAFAFA] p-[16px] gap-[16px] rounded-[16px]">
+                < div className="relative overflow-hidden w-full" >
+                    <div
+                        className={`flex ${isTransitioning ? "transition-transform duration-700 ease-in-out" : ""}`}
+                        style={{ transform: `translateX(-${activeIndex * 100}%)` }
+                        }
+                    >
 
-                    {/* Left Content */}
-                    <div className="flex flex-col gap-[32px] p-[16px] w-full">
-                        <h3 className="font-urbanist font-medium text-[20px] text-[#001066]">
-                            Leading HR Tech Company Cut 80% of Manual Reporting Effort.
-                        </h3>
+                        {
+                            [...caseStudies, caseStudies[0]].map((item, index) => (
 
-                        <p className="font-urbanist font-medium text-[18px] text-[#5C5F70]">
-                            Replaced spreadsheets with a fully managed Apache Superset stack-automating 70+ HR reports in under 3 months.
-                        </p>
 
-                        <button
-                            className="flex items-center gap-[12px] px-4 py-2 sm:px-6 sm:py-3 border border-[#504DFF] text-[#4F46E5] bg-gradient-to-b from-[#FFFFFF] to-[#E0E5FF] rounded-[12px] font-medium text-xs sm:text-sm hover:bg-gray-50 transition w-fit cursor-pointer"
-                        >
-                            <img src={FileIcon} alt="file-icon" className="w-4 h-4 sm:w-5 sm:h-5" />
-                            READ THE FULL STORY
-                        </button>
+                                < div Key={index} className="min-w-full flex flex-col lg:flex-row bg-[#FAFAFA] p-[16px] gap-[16px] rounded-[16px]" >
 
-                        <img src={ToggleIcon} alt="toggle-icon" className="w-[64px] h-[20px]" />
+                                    {/* Left Content */}
+                                    < div className="flex flex-col gap-[32px] p-[16px] w-full" >
+                                        <h3 className="font-urbanist font-medium text-[20px] text-[#001066]">
+                                            {item.title}
+                                        </h3>
+
+                                        <p className="font-urbanist font-medium text-[18px] text-[#5C5F70]">
+                                            {item.description}
+                                        </p>
+
+                                        <button
+                                            className="flex items-center gap-[12px] px-4 py-2 sm:px-6 sm:py-3 border border-[#504DFF] text-[#4F46E5] bg-gradient-to-b from-[#FFFFFF] to-[#E0E5FF] rounded-[12px] font-medium text-xs sm:text-sm hover:bg-gray-50 transition w-fit cursor-pointer"
+                                        >
+                                            <img src={FileIcon} alt="file-icon" className="w-4 h-4 sm:w-5 sm:h-5" />
+                                            READ THE FULL STORY
+                                        </button>
+
+
+                                        {/* Toggle */}
+
+                                        <div
+                                            className="hidden lg:flex relative w-[55px] h-[20px] rounded-[16px] border-1 border-[#504DFF] bg-[#F3F4FF] cursor-pointer flex items-center"
+                                            onClick={() =>
+                                                setActiveIndex((prev) =>
+                                                    prev === caseStudies.length - 1 ? 0 : prev + 1
+                                                )
+                                            }
+                                        >
+                                            {/* Active pill */}
+                                            <div
+                                                className={`absolute h-[12px] w-[24px] rounded-[8px] bg-gradient-to-r from-[#504DFF] to-[#6C63FF] transition-transform duration-500 ${activeIndex % caseStudies.length === 0
+                                                    ? "translate-x-[5px]"
+                                                    : "translate-x-[24px]"
+                                                    }`}
+                                            />
+
+                                            {/* Small dot */}
+                                            <div
+                                                className={`absolute w-[12px] h-[12px] rounded-full bg-[#C5C8FF] transition-transform duration-500 ${activeIndex % caseStudies.length === 0
+                                                    ? "translate-x-[34px]"
+                                                    : "translate-x-[5px]"
+                                                    }`}
+                                            />
+                                        </div>
+
+
+
+                                    </div>
+
+                                    {/* Right Testimonial */}
+                                    <div className="flex flex-col items-center bg-[#FFFFFF] border border-[#E0E5FF] lg:gap-[40px] p-[32px] rounded-[16px] text-center w-full">
+                                        <img src={SwitchIcon} alt="switch-icon" className="h-[48px] w-[55px] mx-auto" />
+
+                                        <p className="font-urbanist font-medium text-[20px] text-[#5C5F70]">
+                                            {item.testimonial}
+                                        </p>
+
+                                        <p className="text-[#1FDA8C] font-urbanist font-semibold text-[16px]">
+                                            {item.author}
+                                        </p>
+                                    </div>
+                                    
+
+                                    {/* toggle  */}
+                                    <div
+                                        className=" flex lg:hidden relative w-[55px] h-[20px] rounded-[16px] border-1 border-[#504DFF] bg-[#F3F4FF] cursor-pointer flex items-center"
+                                        onClick={() =>
+                                            setActiveIndex((prev) =>
+                                                prev === caseStudies.length - 1 ? 0 : prev + 1
+                                            )
+                                        }
+                                    >
+                                        {/* Active pill */}
+                                        <div
+                                            className={`absolute h-[12px] w-[24px] rounded-[8px] bg-gradient-to-r from-[#504DFF] to-[#6C63FF] transition-transform duration-500 ${activeIndex % caseStudies.length === 0
+                                                ? "translate-x-[5px]"
+                                                : "translate-x-[24px]"
+                                                }`}
+                                        />
+
+                                        {/* Small dot */}
+                                        <div
+                                            className={`absolute w-[12px] h-[12px] rounded-full bg-[#C5C8FF] transition-transform duration-500 ${activeIndex % caseStudies.length === 0
+                                                ? "translate-x-[34px]"
+                                                : "translate-x-[5px]"
+                                                }`}
+                                        />
+                                    </div>
+
+
+                                </div>
+                            ))}
                     </div>
 
-                    {/* Right Testimonial */}
-                    <div className="flex flex-col items-center bg-[#FFFFFF] border border-[#E0E5FF] lg:gap-[40px] p-[32px] rounded-[16px] text-center w-full">
-                        <img src={SwitchIcon} alt="switch-icon" className="h-[48px] w-[55px] mx-auto" />
+                </div >
 
-                        <p className="font-urbanist font-medium text-[20px] text-[#5C5F70]">
-                            "With ScaleBI, we finally own our analytics solution and still get enterprise support."
-                        </p>
-
-                        <p className="text-[#1FDA8C] font-urbanist font-semibold text-[16px]">
-                            Director. HR Tech Company
-                        </p>
-                    </div>
-
-                </div>
-            </div>
-        </section>
+            </div >
+        </section >
     );
 }
