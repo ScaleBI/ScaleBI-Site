ac834d198b59363ca8a807723ff3f7c984d3f0dd
Madasamy-123 <madasamy.asokan@ventragate.com>
2026-02-17 17:05:23 +0530

    footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,56 +9,65 @@ import useScrollNavigation from "../hooks/useScrollNavigation";
 
 export default function Footer() {
 
-      const { goToSection } = useScrollNavigation();
+    const { goToSection } = useScrollNavigation();
 
     return (
         <section className="w-full overflow-hidden">
 
             {/* TOP CTA */}
-            <div className="w-full bg-[#504DFF] overflow-hidden">
-
-                <div className="flex flex-col xl:flex-row mt-[40px] xl:mt-0">
-
-                    {/* LEFT CONTENT */}
-
-                    <div className="w-full w-[1280px] text-center xl:text-left flex flex-col gap-8  xl:py-[80px] xl:pl-[120px] xl:whitespace-nowrap">
-                        <h2 className="font-urbanist font-semibold text-[28px] sm:text-[32px] xl:text-[40px] text-white">
-                            Let's Build BI That You Own
-                        </h2>
-
-                        <p className="font-urbanist font-medium text-[16px] sm:text-[18px] lg:text-[20px] text-white">
-                            Book a free consultation with our BI architects and <br />
-                            start owning your data, not renting it.
-                        </p>
-
-                        <button className="flex items-center gap-3 py-3 px-5 bg-gradient-to-b from-white to-[#E0E5FF] text-[#4F46E5] rounded-xl font-jetbrains text-sm font-medium w-fit mx-auto xl:mx-0 hover:opacity-90 transition cursor-pointer">
-                            <img src={ArrowIcon} alt="arrow" className="w-4 h-4" />
-                            REQUEST CONSULTATION
-                        </button>
-
-                        <p className="font-jetbrains text-[14px] sm:text-[16px] lg:text-[18px] uppercase text-white">
-                            NO COMMITMENTS, JUST A 20-MINUTE STRATEGY SESSION WITH OUR BI EXPERTS.
-                        </p>
-
-                    </div>
-
-                    {/* </div> */}
+            <div className="w-full bg-[#504DFF] overflow-hidden relative ">
 
 
+                <div className="max-w-[1440px] mx-auto px-6 xl:px-[80px] py-[80px]">
 
-                    {/* IMAGE OUTSIDE CONTAINER */}
-                    <div className="w-full  flex justify-end items-end">
-                        <img
-                            src={DashBoardImage}
-                            alt="dashboard"
-                            className="w-[90%] sm:w-[75%] md:w-[90%] xl:w-[550px]  mt-[40px] sm:mt-[60px] xl:mt-[80px] object-contain
-        "
-                        />
+
+                    <div className="flex flex-col xl:flex-row">
+
+                        {/* LEFT CONTENT */}
+
+
+                        <div className="w-full text-center xl:text-left flex flex-col gap-8 xl:whitespace-nowrap">
+
+                            <h2 className="font-urbanist font-semibold text-[28px] sm:text-[32px] xl:text-[40px] text-white">
+                                Let's Build BI That You Own
+                            </h2>
+
+                            <p className="font-urbanist font-medium text-[16px] sm:text-[18px] lg:text-[20px] text-white">
+                                Book a free consultation with our BI architects and <br />
+                                start owning your data, not renting it.
+                            </p>
+
+                            <button className="flex items-center gap-3 py-3 px-5 bg-gradient-to-b from-white to-[#E0E5FF] text-[#4F46E5] rounded-xl font-jetbrains text-sm font-medium w-fit mx-auto xl:mx-0 hover:opacity-90 transition cursor-pointer">
+                                <img src={ArrowIcon} alt="arrow" className="w-4 h-4" />
+                                REQUEST CONSULTATION
+                            </button>
+
+                            <p className="font-jetbrains text-[14px] sm:text-[16px] lg:text-[18px] uppercase text-white">
+                                NO COMMITMENTS, JUST A 20-MINUTE STRATEGY SESSION WITH OUR BI EXPERTS.
+                            </p>
+
+                        </div>
                     </div>
 
                 </div>
 
+
+
+                {/* IMAGE OUTSIDE CONTAINER */}
+                <div className="w-full lg:flex lg:justify-end xl:absolute xl:right-0 xl:top-[80px]">
+                    <img
+                        src={DashBoardImage}
+                        alt="dashboard"
+                        className="w-full pl-16 sm:pl-8 md:pl-20 lg:pl-20  xl:w-[600px]  object-contain"
+                    />
+                </div>
+
+
+
+
             </div>
+
+
 
             {/* BOTTOM DARK SECTION */}
             <div className="w-full bg-[#000419]">
@@ -71,12 +80,12 @@ export default function Footer() {
 
                         <div className="bg-white py-2 px-6 rounded-xl w-full xl:w-auto">
                             <ul className="flex flex-col xl:flex-row items-center items-center gap-4 py-1 px-4 font-jetbrains uppercase text-[#5C5F70] text-[14px] lg:text-[16px]">
-                                <li className="hover:text-[#504DFF] cursor-pointer lg:px-4" onClick={()=>goToSection('hero')}>Home</li>
-                                <li className="hover:text-[#504DFF] cursor-pointer lg:px-4" onClick={()=>goToSection('comparison')}>Why Us</li>
+                                <li className="hover:text-[#504DFF] cursor-pointer lg:px-4" onClick={() => goToSection('hero')}>Home</li>
+                                <li className="hover:text-[#504DFF] cursor-pointer lg:px-4" onClick={() => goToSection('comparison')}>Why Us</li>
                                 <li className="hover:text-[#504DFF] cursor-pointer lg:px-4">
-                                   <Link to="/case-study-index">
-                                   Success Stories
-                                   </Link> </li>
+                                    <Link to="/case-study-index">
+                                        Success Stories
+                                    </Link> </li>
                                 <li className="hover:text-[#504DFF] cursor-pointer lg:px-4">Contact Us</li>
                             </ul>
                         </div>
@@ -99,6 +108,6 @@ export default function Footer() {
 
             </div>
 
-        </section>
+        </section >
     );
 }
