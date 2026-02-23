7339061cbb2d6ae84fac63d648a6ff9d2d626e87
Madasamy-123 <madasamy.asokan@ventragate.com>
2026-02-23 13:28:38 +0530

    deploy fix issue

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/ScaleBI-Site">
     <App />
   </BrowserRouter>
 )
