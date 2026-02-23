698abd0cbe834396ecf261ba2c8f1a6c0eca66e8
Madasamy-123 <madasamy.asokan@ventragate.com>
2026-02-23 18:38:26 +0530

    remove it useless code

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const redirect = sessionStorage.redirect;
-if (redirect) {
-  sessionStorage.removeItem("redirect");
-  window.history.replaceState(null, null, redirect);
-}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
